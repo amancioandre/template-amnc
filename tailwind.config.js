99b92e455fcbd0476b93de9f9c57bc117ce4039a
Andre Moraes <amancioandre@gmail.com>
2021-08-29 16:07:41 -0300

    feat: add copyToClipboard and animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@ module.exports = {
   purge: ["layouts/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+        keyframes: {
+            'fade-in-up': {
+                "0%": {
+                    opacity: '0',
+                    transform: 'translateY(10px)'
+                },
+                "100%": {
+                    opacity: '1',
+                    transform: 'translateY(0)'
+                }
+            }
+        },
+        animation: {
+            'fade-in-up': 'fade-in-up 0.5s ease-out'
+        }
+    },
   },
   variants: {
     extend: {
